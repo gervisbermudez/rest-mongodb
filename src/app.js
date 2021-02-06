3694a5f14e5569ec3927ecd4dd03f58b1739837c
Gervis Bermudez <gervisbermudez15@gmail.com>
2021-02-06 16:47:20 -0300

    add new routes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv/config");
+
+//Import Routes
+const postRoutes = require('./routes/posts');
+
+app.use('/posts', postRoutes);
 
 //Routes
 app.get('/', (req, res) => {
     res.send("Hello Word");
-})
+});
+
+//Conect to DB
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => { console.log("Connected with DB!"); });
 
 app.listen('3000');
