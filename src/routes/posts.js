eb55ca5718cb6da063726b6ecea4866469a85498
Gervis Bermudez <gervisbermudez15@gmail.com>
2021-02-07 20:15:16 -0300

    add Post models

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -2,7 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
+const Post = require('../models/Post');
+
 router.get('/', (req, res) => {
+    res.send("/posts")
+});
+
+router.post('/', (req, res) => {
+    console.log(req.body);
     res.send("/posts")
 });
 
